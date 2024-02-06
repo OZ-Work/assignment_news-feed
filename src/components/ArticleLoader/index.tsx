891318cecf420415a28f8f42b1dc0316d8e465f6
OZaldea-OVO <oleg.zaldea@ovo.com>
2024-02-06 22:13:12 +0200

    cleanup

--- src/components/ArticleLoader/index.tsx
+++ src/components/ArticleLoader/index.tsx
@@ -1,6 +1,6 @@
 import { FlexGrowStyled, FlexStyled } from "styles/containers.styles";
 import { ImageContainerStyled } from "styles/components.styles";
-import { default as LoadingCard } from "react-loading-skeleton";
+import { default as Loader } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 
 export default function ArticleLoader() {
@@ -9,12 +9,12 @@ export default function ArticleLoader() {
       <ImageContainerStyled
         $width={{ smallDown: 144, smallUp: 200, largeUp: 240 }}
       >
-        <LoadingCard count={1} height={100} />
+        <Loader count={1} height={100} />
       </ImageContainerStyled>
       <FlexGrowStyled>
-        <LoadingCard count={1} height={45} />
-        <LoadingCard count={1} height={30} />
-        <LoadingCard count={1} height={20} />
+        <Loader count={1} height={45} />
+        <Loader count={1} height={30} />
+        <Loader count={1} height={20} />
       </FlexGrowStyled>
     </FlexStyled>
   );
